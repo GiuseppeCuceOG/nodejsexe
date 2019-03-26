f8e4d1b3daf15c22a8ccb2c444d673ac4b05756d
Giuseppe Cuce <giuseppecuce.og@gmail.com>
2019-03-26 16:54:25 +0200

    1st Assignment

--- node-express/index.js
+++ node-express/index.js
@@ -4,6 +4,8 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -33,6 +35,8 @@ app.delete('/dishes/:dishId', (req, res, next) => {
 });*/
 
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 
 app.use(express.static(__dirname+ '/public'));
 
